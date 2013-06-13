0ea43841f703e3d7c67b2473a2f0a556031b7a55
Neng Xu <neng2.xu2@gmail.com>
2013-06-13 11:47:07 -0700

    Abort execution when locked.

--- lib/dolphin/lock.rb
+++ lib/dolphin/lock.rb
@@ -1,15 +1,14 @@
-# Lock resource to avoid simultaneous deployments
+# Use lock to avoid simultaneous deployments
 class Dolphin::Lock < Dolphin::Base
 
   desc "check", "Check lock"
   def check
     command = "if [ -e #{@lock_file} ]; then echo 'true'; fi"
     if 'true' ==  capture(command, @lead_server).strip
-      puts "\n\n\n\e[0;31m A deployment is already in progress\n Please wait for its completion\nOr in case of stale lock, remove #{@lock_file} to unlock \e[0m\n\n\n"
+      abort "\n\n\n\e[0;31m A deployment is already in progress\n Please wait for its completion\nOr in case of stale lock, remove #{@lock_file} to unlock \e[0m\n\n\n"
     else
-      puts "ok to proceed"
+      puts "OK to proceed"
     end
-
   end
 
   desc "create", "Create lock"
