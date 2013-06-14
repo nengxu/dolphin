bab9a102f40517394ae562dba0f712a7a5452adc
Neng Xu <neng2.xu2@gmail.com>
2013-06-14 11:27:27 -0700

    Added generator dolphin:install.

---
+++ lib/generators/dolphin/install_generator.rb
@@ -0,0 +1,14 @@
+require 'rails/generators'
+
+class Dolphin::InstallGenerator < Rails::Generators::Base
+  source_root File.expand_path("../templates", __FILE__)
+
+  def setup
+    target = "bin/dolphin"
+    copy_file "dolphin", target
+    # make it executable
+    chmod(target, 0755)
+    puts "Now edit bin/dolphin to adjust deployment settings\nThen run bin/dolphin to deploy."
+  end
+
+end
